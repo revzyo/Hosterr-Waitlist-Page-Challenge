c61094a91209d68f8a4fce3a804c8b92c3b3ab70
revzyo <raghavdixitdixit@gmail.com>
2024-04-21 22:55:05 +0530

    header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-const Header = () =>{
-    return (
-        <div>
-            <div>
-                    <img className="max-w-10" src="./assets/logo.svg" alt="logo" />
-                    <button>Hoster is Hiring</button>
-            </div>
-            <div>
+import { FaBars } from "react-icons/fa6";
 
-            </div>
-        </div>
-
-
-        
-    )
-}
-export default Header
+const Header = () => {
+  return (
+    <div className="flex justify-between">
+      <div className="flex items-center justify-center gap-2 ">
+        <img className="max-w-10" src="./assets/logo.svg" alt="logo" />
+        <button className="bg-gradient-to-r from-orange-400 to-red-400 rounded-3xl px-4 text-xs text-white py-1.5">Hosterr is Hiring!</button>
+      </div>
+      <div>
+        <FaBars />
+      </div>
+    </div>
+  );
+};
+export default Header;
