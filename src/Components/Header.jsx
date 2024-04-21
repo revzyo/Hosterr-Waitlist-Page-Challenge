71404835ba2e784d90b156805791593efd1d6840
revzyo <raghavdixitdixit@gmail.com>
2024-04-21 22:26:18 +0530

    $$

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 const Header = () =>{
     return (
-        <div>Header</div>
+        <div>
+            <div>
+                    <img className="max-w-10" src="./assets/logo.svg" alt="logo" />
+                    <button>Hoster is Hiring</button>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+
+        
     )
 }
 export default Header
