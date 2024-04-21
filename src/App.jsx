6af6bb44e7b7d5082a3e66aa4262449c9e8d0227
revzyo <raghavdixitdixit@gmail.com>
2024-04-21 22:07:21 +0530

    $$

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-amber-50">
+      <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16">
         <Header/>
         <Body/>
         <Footer/>
