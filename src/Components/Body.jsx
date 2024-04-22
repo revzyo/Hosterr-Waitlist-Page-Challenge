103521a22c07c5494e60a59ba70673cffab83698
revzyo <raghavdixitdixit@gmail.com>
2024-04-22 11:01:55 +0530

    final commit

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,34 +2,34 @@ import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
 const Body = () => {
   return (
-    <div>
-      <div className="flex items-center">
-        <img src="./assets/blue.svg" alt="" className="-rotate-45 h-64" />
+    <div className="lg:flex">
+      <div className="flex items-center justify-center lg:flex-1 lg:order-2 lg:justify-end ">
+        <img src="./assets/blue.svg" alt="" className="-rotate-45 h-64 md:h-72 lg:h-[400px]" />
         <img
           src="./assets/red.svg"
           alt=""
-          className="absolute -rotate-[30deg] h-64"
+          className="absolute -rotate-[30deg] h-64 md:h-72 lg:h-[400px]"
         />
         <img
           src="./assets/purple.svg"
           alt=""
-          className="absolute -rotate-[15deg] h-64"
+          className="absolute -rotate-[15deg] h-64 md:h-72 lg:h-[400px]"
         />
         <img
           src="./assets/LogoMain.png"
           alt=""
-          className="absolute rounded-s-2xl h-64 "
+          className="absolute rounded-s-2xl h-64 md:h-72 lg:h-[400px]"
         />
       </div>
-      <div>
+      <div className="lg:flex-1 lg:order-1">
         <h1 className="text-5xl font-bold font-playfair leading-tight mt-10">Host your website in less than 5 minutes.</h1>
         <p className="font-lato text-gray-400 mt-3">
           With Hosterr, get your website up and running in no less than 5
           minutes with the most competitive pricing packages available online.{" "}
         </p>
-        <form action="" className="flex flex-col gap-4 mt-3">
+        <form action="" className="flex flex-col gap-4 mt-3 md:flex-row">
           <input className="rounded-md px-4 py-3 placeholder:text-gray-400 " type="email" placeholder="Enter e-mail address" />
-          <button className="rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-700 text-white">Join Waitlist</button>
+          <button  >Join Waitlist</button>
         </form>
         <div className="flex gap-2">
           <FaCheckCircle className="mt-1" />
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,16 @@ const Header = () => {
         <img className="max-w-10" src="./assets/logo.svg" alt="logo" />
         <button className="bg-gradient-to-r from-orange-400 to-red-400 rounded-3xl px-4 text-xs text-white py-1.5">Hosterr is Hiring!</button>
       </div>
-      <div>
+      <ul className="hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6">
+        <li><a href="#"></a>Plans</li>
+        <li><a href="#"></a>Find Domain</li>
+        <li><a href="#"></a>Why Hosterr?</li>
+      </ul>
+      <div className="hidden lg:flex justify-center items-center font-lato gap-6 ">
+        <a className="text-gray-400" href="#">SignIn </a>
+        <button className="rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-700 text-white" >Join WaitList</button>
+      </div>
+      <div className="lg:hidden">
         <FaBars />
       </div>
     </div>
