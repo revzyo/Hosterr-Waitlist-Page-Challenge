a2520ee48757bf2cd13dc7a2f5704335e5b8033e
revzyo <raghavdixitdixit@gmail.com>
2024-04-22 10:30:54 +0530

    Footer Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 const Footer = () =>{
     return (
-        <div>Footer</div>
+        <div className="flex flex-col gap-12">
+            
+            <ul className="flex gap-6 font-lato text-gray-400">
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Twitter</a></li>
+            </ul> 
+            
+            <div className="flex gap-2">
+                <img  className="h-11" src="./assets/icon.svg" alt="icon" />
+                <p className="font-playfair font-thin">Have any questions?</p>
+                <a className="font-lato font-medium" href="#">Talk to a specialist</a>
+            </div>
+            
+        </div>
     )
 }
-export default Footer
+export default Footer 
