fc116e839b8b6f9d65a68786bc1e36d78b14b3b6
revzyo <raghavdixitdixit@gmail.com>
2024-04-22 10:12:40 +0530

    Body Done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -22,18 +22,18 @@ const Body = () => {
         />
       </div>
       <div>
-        <h1 className="">Host your website in less than 5 minutes.</h1>
-        <p>
+        <h1 className="text-5xl font-bold font-playfair leading-tight mt-10">Host your website in less than 5 minutes.</h1>
+        <p className="font-lato text-gray-400 mt-3">
           With Hosterr, get your website up and running in no less than 5
           minutes with the most competitive pricing packages available online.{" "}
         </p>
-        <form action="">
-          <input type="email" placeholder="Enter e-mail address" />
-          <button>Join Waitlist</button>
+        <form action="" className="flex flex-col gap-4 mt-3">
+          <input className="rounded-md px-4 py-3 placeholder:text-gray-400 " type="email" placeholder="Enter e-mail address" />
+          <button className="rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-700 text-white">Join Waitlist</button>
         </form>
-        <div>
-          <FaCheckCircle />
-          <p>No spam, ever. Unsubscribe anytime.</p>
+        <div className="flex gap-2">
+          <FaCheckCircle className="mt-1" />
+          <p className="font-lato text-gray-400">No spam, ever. Unsubscribe anytime.</p>
         </div>
       </div>
     </div>
