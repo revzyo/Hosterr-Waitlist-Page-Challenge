01b1e6d372875826c1f3fc8854c2bb6a0315096b
revzyo <raghavdixitdixit@gmail.com>
2024-04-22 09:31:58 +0530

    $$

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 const Body = () =>{
     return (
-        <div>Body</div>
+        <div>
+            <div className="flex items-center">
+                <img src="./assets/blue.svg" alt="" className="-rotate-45 h-64" />
+                <img src="./assets/red.svg" alt="" className="absolute -rotate-[30deg] h-64"  />
+                <img src="./assets/purple.svg" alt="" className="absolute -rotate-[15deg] h-64" />
+                <img src="./assets/LogoMain.png" alt="" className="absolute rounded-s-2xl h-64 " />
+            </div>
+            <div></div>
+        </div>
     )
 }
 export default Body
